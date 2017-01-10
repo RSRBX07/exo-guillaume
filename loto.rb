63070dcdd7b25dc281179ed74f4066882a44e184
Guillaume <nomine.g@gmail.com>
2017-01-10 14:56:47 +0100

    add

--- loto.rb
+++ loto.rb
@@ -3,3 +3,17 @@
 #5.times do |i| tirage.push 1 + rand(45) end
 #puts tirage.inspect
 
+#affichage du montant (entre 100 & 500k€)
+#puts "la cagnote est : #{cagnote}"
+cagnote = 100.000
+
+#le vendredi 13 la cagnote est de 2m
+vendredi_13 = true
+
+if vendredi_13
+    cagnote = 2.000.000
+    else 
+    cagnote = 100.000
+end
+
+puts "la cagnote est de #{cagnote}"
