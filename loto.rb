a543969e0a22e0d0314bb4c84093523fee9d0e7f
Guillaume <nomine.g@gmail.com>
2017-01-10 14:41:16 +0100

    add

--- loto.rb
+++ loto.rb
@@ -1,6 +1,5 @@
 #afficher un tirage de 5 nombres entre 1-45 du loto
-tirage = []
+#tirage = []
+#5.times do |i| tirage.push 1 + rand(45) end
+#puts tirage.inspect
 
-5.times do |i| tirage.push 1 + rand(45) end
-
-puts tirage.inspect
