ebafd90d230f9a4f6d2fedf241aa1f59d5bd4ccf
Guillaume <nomine.g@gmail.com>
2017-01-10 16:28:09 +0100

    song

---
+++ song.rb
@@ -0,0 +1,12 @@
+Ma poul' n'a plus qu' vingt-neuf poussins, 
+Ma poul' n'a plus qu' vingt-neuf poussins,
+Elle en avait trente.
+Allongeons la jambe,
+Allongeons la jambe, la jambe
+Car la route est longue.
+Allongeons la jambe, la jambe
+Car la route est longue 
+
+
+
+puts 
