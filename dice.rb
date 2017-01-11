a0f979dc3c24c18b5fe7bf57758bdafe964aa89b
Guillaume <nomine.g@gmail.com>
2017-01-11 16:18:43 +0100

    methode roll

--- dice.rb
+++ dice.rb
@@ -1,4 +1,12 @@
 # afficher resultat de dés à 6 faces
-faces = [1,2,3,4,5,6]
-roll =faces[rand 6]
-puts roll
+#faces = [1,2,3,4,5,6]
+#roll =faces[rand 6]
+#puts roll
+
+#ecrire une methode roll pour retourner comptenu du dés
+def roll
+    faces = [1,2,3,4,5,6]
+    index =rand 0..5
+    return faces [index]
+end   
+    puts roll
