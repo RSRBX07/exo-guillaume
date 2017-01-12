b4e89d0019331df3155310d633cf712c8f6be967
Guillaume <nomine.g@gmail.com>
2017-01-12 14:13:41 +0100

    french_numbers work in progress

---
+++ french_numbers.rb
@@ -0,0 +1,9 @@
+#ecrire une methode qui prend une instance de fixnum en argument (accepte de 0 à 9)
+#retourne la chaine de caractere du nombre ecrit en toute lettre
+
+def nombre
+    chiffre = (0..9).to_s
+
+put chiffre
+
+
