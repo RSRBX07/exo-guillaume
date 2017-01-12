af070a405410883036e0392704e82d23368cce59
Guillaume <nomine.g@gmail.com>
2017-01-12 14:59:38 +0100

    french numbers tableau

--- french_numbers.rb
+++ french_numbers.rb
@@ -1,9 +1,52 @@
+=begin
 #ecrire une methode qui prend une instance de fixnum en argument (accepte de 0 à 9)
 #retourne la chaine de caractere du nombre ecrit en toute lettre
+#methode
 
-def nombre
-    chiffre = (0..9).to_s
+def nombre num
+case num
 
-put chiffre
+when 0
+    "zero"
+when 1
+    "un"
+when 2
+    "deux"
+when 3
+    "trois"
+when 4
+    "quatre"
+when 5
+    "cinq"
+when 6
+    "six"
+when 7
+    "sept"
+when 8
+    "huit"
+when 9
+    "neuf"
+else 
+    nil
+end 
+end
+
+puts nombre num 1
+=end
+
+
+def in_french num       
+    tte_lettres = ["zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"]
+    return tte_lettres [num]
+end 
+
+puts("Choisir un chiffre entre 0 et 9")
+num = gets.chomp.to_i
+
+puts in_french num
+
+
+
+
 
 
