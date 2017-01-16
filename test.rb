d04f0f46c37a716bb282bd03b6dc8bcee03ef29d
Guillaume <nomine.g@gmail.com>
2017-01-16 10:32:34 +0100

    class loto

---
+++ test.rb
@@ -0,0 +1,22 @@
+
+
+# #création de class
+# class Dice 
+#     def roll (cheated_value = false)
+#     cheated_value
+# end
+
+#test de dice.rb
+# require './dice.rb'
+
+# my_dice = Dice.new
+
+#     puts my_dice.roll
+#     puts my_dice.roll 4
+ 
+
+ #test de loto
+ require './loto.rb'
+
+    my_loto = Loto.new
+    puts my_loto.tirage
