dd245e053e800d9c7a53f6d1290bb92cd0df6003
Guillaume <nomine.g@gmail.com>
2017-01-16 16:52:58 +0100

    new french numbers

--- french_numbers.rb
+++ french_numbers.rb
@@ -45,21 +45,12 @@ num = gets.chomp.to_i
 
 puts in_french num
 =end
-
+class French_number
 def in_french num       
     tte_lettres = ["zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix", "onze", "douze", "treize", "quatorze", "quinze", "seize"]
-    return tte_lettres [num]
-
+    return tte_lettres[num]
 end
-end 
+end
 
-puts("Choisir un chiffre entre 0 et 19")
-num = gets.chomp.to_i
-
-puts in_french num
-
-
-
-
-
-
+    #puts("Choisir un chiffre entre 0 et 19")
+    #num = gets.chomp.to_i
