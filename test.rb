6dbaf7f6656a43f7cbee8fc64d9be21007dad5e1
Guillaume <nomine.g@gmail.com>
2017-01-16 16:51:04 +0100

    add french_numbers

--- test.rb
+++ test.rb
@@ -15,8 +15,18 @@
 #     puts my_dice.roll 4
  
 
- #test de loto
- require './loto.rb'
 
-    my_loto = Loto.new
-    puts my_loto.tirage
+
+#Loto
+#test tirage 5 nombres de loto
+# require './loto2.rb'
+
+#     my_loto = Loto.new
+#     #puts my_loto.tirage
+
+# my_loto.validate_grid Loto.get_grid
+
+require './french_numbers.rb'
+
+    my_numbers = French_number.new
+    puts my_numbers.in_french gets.chomp.to_i
