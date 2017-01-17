e2471e673efb4913ddeb9349ecbc933e0f6d8adf
Guillaume <nomine.g@gmail.com>
2017-01-17 09:45:28 +0100

    test respond.to

--- french_numbers.rb
+++ french_numbers.rb
@@ -45,11 +45,12 @@ num = gets.chomp.to_i
 
 puts in_french num
 =end
-class French_number
-def in_french num       
-    tte_lettres = ["zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix", "onze", "douze", "treize", "quatorze", "quinze", "seize"]
-    return tte_lettres[num]
-end
+ 
+class Fixnum
+    def in_french num       
+        tte_lettres = ["zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix", "onze", "douze", "treize", "quatorze", "quinze", "seize"]
+        return tte_lettres[num]
+    end
 end
 
     #puts("Choisir un chiffre entre 0 et 19")
