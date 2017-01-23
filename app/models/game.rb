531264e5e6e47c9478bbeb2fcd4c89a817e2b0e2
Guillaume <nomine.g@gmail.com>
2017-01-23 16:44:32 +0100

    methode migration

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,17 @@
 class Game < ApplicationRecord
+
+
+  def add_player
+    @players +=1 
+  end 
+
+  def remove_player
+    raise RuntimeError "no more player to removed" if players <= 0
+    @players -=1
+  end
+
+private
+  def initialize
+    @players = 0 
+  end
 end
