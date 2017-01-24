fc2d875fc65f57bffae713f1f7b777b1462ddf5b
Guillaume <nomine.g@gmail.com>
2017-01-24 11:28:43 +0100

    seeds loto

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Game.create ([
+  {name: 'loto du samedi', description: "Le tirage du jour est : 1, 2, 3, 4, 5", players: 224598},
+  {name: 'loto du samedi', description: "Le tirage du jour est : 44, 22, 36, 4, 5", players: 224498},
+  {name: 'loto du samedi', description: "Le tirage du jour est : 1, 12, 33, 39, 15", players: 224898},
+  {name: 'loto du samedi', description: "Le tirage du jour est : 17, 26, 3, 4, 35", players: 222598},
+  {name: 'loto du samedi', description: "Le tirage du jour est : 10, 2, 23, 44, 15", players: 221598}
+])
