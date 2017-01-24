fbc36d08e86cd741283ab75945d7b8ddbdc80f6e
Guillaume <nomine.g@gmail.com>
2017-01-24 12:06:36 +0100

    liste de jeu

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Game.create ([
-  {name: 'loto du samedi', description: "Le tirage du jour est : 1, 2, 3, 4, 5", players: 224598},
-  {name: 'loto du samedi', description: "Le tirage du jour est : 44, 22, 36, 4, 5", players: 224498},
-  {name: 'loto du samedi', description: "Le tirage du jour est : 1, 12, 33, 39, 15", players: 224898},
-  {name: 'loto du samedi', description: "Le tirage du jour est : 17, 26, 3, 4, 35", players: 222598},
-  {name: 'loto du samedi', description: "Le tirage du jour est : 10, 2, 23, 44, 15", players: 221598}
+  {name: 'loto du Lundi', description: "Le tirage du jour est : 1, 2, 3, 4, 5", players: 224598},
+  {name: 'loto du Mardi', description: "Le tirage du jour est : 44, 22, 36, 4, 5", players: 224498},
+  {name: 'loto du Mercredi', description: "Le tirage du jour est : 1, 12, 33, 39, 15", players: 224898},
+  {name: 'loto du Jeudi', description: "Le tirage du jour est : 17, 26, 3, 4, 35", players: 222598},
+  {name: 'loto du Vendredi', description: "Le tirage du jour est : 10, 2, 23, 44, 15", players: 221598},
+  {name: 'loto du Samedi', description: "Le tirage du jour est : 16, 42, 28, 44, 35", players: 221597}
 ])
